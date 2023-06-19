d4932091ab62841365112b1d9aa5cebd6ef831da
Chetan Choudhary <98541541+AlfieSolomons09@users.noreply.github.com>
2023-06-19 20:30:38 +0530

    Initial Commit

---
+++ SwiftfulFirebaseBootcamp/SwiftfulFirebaseBootcampApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftfulFirebaseBootcampApp.swift
+//  SwiftfulFirebaseBootcamp
+//
+//  Created by Chetan Choudhary on 19/06/23.
+//
+
+import SwiftUI
+
+@main
+struct SwiftfulFirebaseBootcampApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
